1843ae1192b421adaed881a2f633d42d5a18281e
NimbusScale <joe@nimbusscale.com>
2017-07-26 10:38:30 -0700

    inital

---
+++ functions/get_items_by_user/get_items_by_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import logging
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+table = dynamodb.Table('items')
+
+
+def lambda_handler(event,context):
+    response = table.query(
+        KeyConditionExpression=Key('user_id').eq(event['user_id'])
+    )
+    return(response)
