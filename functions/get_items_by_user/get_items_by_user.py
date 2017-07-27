dedc3afa0c0b7da5d1c0dfb687746a5ce6a79df3
NimbusScale <joe@nimbusscale.com>
2017-07-26 18:17:14 -0700

    paramterize region

--- functions/get_items_by_user/get_items_by_user.py
+++ functions/get_items_by_user/get_items_by_user.py
@@ -5,13 +5,14 @@ import os
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+region = os.environ['AWS_DEFAULT_REGION']
+dynamodb = boto3.resource('dynamodb', region_name=region)
 table = dynamodb.Table(os.environ['itemsTableName'])
 
 
 def lambda_handler(event,context):
     response = table.query(
-        KeyConditionExpression=Key('user_id').eq(event['user_id'])
+        KeyConditionExpression = Key('user_id').eq(event['user_id'])
     )
     return(response)
 
