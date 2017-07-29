d89a65095bee4f80b56655a4ff407c42181bc79a
NimbusScale <joe@nimbusscale.com>
2017-07-28 19:41:46 -0700

    unit testing in pipeline

--- testing/unittest.sh
+++ testing/unittest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-DYNDB_PATH=/home/jjk3/dynamodb
+DYNDB_PATH=./dynamodb
+
+pip3 install -r ../requirements.txt
+
+if [[ ! -d ${DYNDB_PATH} ]]; then
+    wget https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip
+    unzip -q -d ${DYNDB_PATH} dynamodb_local_latest.zip
+fi
 
 # Start Local DynamoDB
 java -Djava.library.path=${DYNDB_PATH}/DynamoDBLocal_lib -jar ${DYNDB_PATH}/DynamoDBLocal.jar -sharedDb -inMemory &
