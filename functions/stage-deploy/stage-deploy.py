ee89392aa0a2e617d60563ecf37de46f1c0a0ca0
NimbusScale <joe@nimbusscale.com>
2017-08-02 12:49:13 -0700

    lambda based API Deploy

--- functions/stage-deploy/stage-deploy.py
+++ functions/stage-deploy/stage-deploy.py
@@ -2,13 +2,18 @@
 import json
 import boto3
 
-
+# Todo add better error handling and Logging
 def lambda_handler(event, context):
+    apigw = boto3.client('apigateway')
+    cloudformation = boto3.resource('cloudformation')
+    codepipeline = boto3.client('codepipeline')
+    # Get CodePipeline Job Info
+    job_id = event['CodePipeline.job']['id']
     job_data = event['CodePipeline.job']['data']
     job_config = job_data['actionConfiguration']['configuration']
     user_params = json.loads(job_config['UserParameters'])
-    apigw = boto3.client('apigateway')
-    cloudformation = boto3.resource('cloudformation')
+    # Get apiGwId from cloudformation stack. The stack name is defined as a
+    # parameter in the DeployApi Invoke Action in CodeDeploy Deploy Stack stage
     cfn_stack = cloudformation.Stack(user_params['stackName'])
     apiGwId = None
     for output in cfn_stack.outputs:
@@ -17,5 +22,8 @@ def lambda_handler(event, context):
     if apiGwId is None:
         raise Exception("apiGwId is not defined as an output for "
                 "the stack {}".format(user_params['stackName']))
+    # Deploy the API
     apigw.create_deployment(restApiId=apiGwId,
                             stageName=user_params['stageName'])
+    # Confirm Success back to codepipeline
+    codepipeline.put_job_success_result(jobId=job_id)
