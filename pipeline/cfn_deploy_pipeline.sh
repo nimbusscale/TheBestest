c723c2fdd10dcfddc1383f87dca41274c3df5c20
NimbusScale <joe@nimbusscale.com>
2017-08-03 16:08:15 -0700

    splitup pipeline

--- pipeline/cfn_deploy_pipeline.sh
+++ pipeline/cfn_deploy_pipeline.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "## Validate"
-aws cloudformation validate-template --template-body file://pipeline.yaml
+aws cloudformation validate-template --template-body file://pipeline_stack.yaml
 
 echo "## Package"
-aws cloudformation package --template-file pipeline.yaml --s3-bucket thebestest-pipeline --output-template-file pipeline_deploy.yaml
+aws cloudformation package --template-file pipeline_stack.yaml --s3-bucket thebestest-pipeline --output-template-file pipeline_stack_deploy.yaml
 
 echo "## Deploy"
-aws cloudformation deploy --template-file pipeline_deploy.yaml --stack-name thebestest-pipeline --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file pipeline_stack_deploy.yaml --stack-name thebestest-pipeline --capabilities CAPABILITY_IAM
