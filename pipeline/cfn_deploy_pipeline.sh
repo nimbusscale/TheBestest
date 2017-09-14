1d9053aa11b65ffb9c87dee9cc4e39a900c3e927
NimbusScale <joe@nimbusscale.com>
2017-09-14 14:06:30 -0700

    split and paramaterized test and deploy pipelines

--- pipeline/cfn_deploy_pipeline.sh
+++ pipeline/cfn_deploy_pipeline.sh
@@ -1,11 +1,69 @@
 #!/bin/bash
 set -e
 
+
+
+function usage() {
+cat <<EOM
+Usage: `basename $0` --pipeline [value] --env [value] [OPTION]
+Arguments:
+    --pipeline          Pipeline type: test or deploy (defaults to test)
+    --branch            Git branch to checkout (defaults to master)
+    --env               Environment name (defaults to sanitized branch name)
+EOM
+
+exit
+}
+
+function arg_parse() {
+# Default params, maybe overrided by args
+PIPELINE="test"
+GIT_BRANCH="master"
+ENV=${GIT_BRANCH//[\_\.]/-}
+
+# Parse Args
+while [[ $# -gt 1 ]]
+do
+    ARG="$1"
+
+    case $ARG in
+        -b|--branch)
+        GIT_BRANCH="$2"
+        shift
+        ;;
+        -e|--env)
+        ENV="$2"
+        shift
+        ;;
+        -p|--pipeline)
+        PIPELINE="$2"
+        shift
+        ;;
+        *)
+        usage
+        ;;
+    esac
+    shift
+done
+}
+
+function cfn_deploy() {
+STACK_NAME="thebestest-${ENV}-${PIPELINE}-pipeline"
+
 echo "## Validate"
-aws cloudformation validate-template --template-body file://pipeline_stack.yaml
+aws cloudformation validate-template --template-body file://pipeline_${PIPELINE}_stack.yaml
 
 echo "## Package"
-aws cloudformation package --template-file pipeline_stack.yaml --s3-bucket thebestest-pipeline --output-template-file pipeline_stack_deploy.yaml
+aws cloudformation package  --template-file pipeline_${PIPELINE}_stack.yaml \
+                            --s3-bucket thebestest-pipeline \
+                            --output-template-file pipeline_stack_${PIPELINE}_deploy.yaml
 
 echo "## Deploy"
-aws cloudformation deploy --template-file pipeline_stack_deploy.yaml --stack-name thebestest-pipeline --capabilities CAPABILITY_IAM
+aws cloudformation deploy   --template-file pipeline_stack_${PIPELINE}_deploy.yaml \
+                            --stack-name ${STACK_NAME} \
+                            --capabilities CAPABILITY_IAM \
+                            --parameter-overrides "gitBranch"=${GIT_BRANCH} "envName"=${ENV}
+}
+
+arg_parse $@
+cfn_deploy
