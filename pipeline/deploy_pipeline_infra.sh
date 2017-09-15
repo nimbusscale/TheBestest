5ba7d03a19f471bc5b12de857e47726b32e3e8b4
NimbusScale <joe@nimbusscale.com>
2017-09-14 18:17:11 -0700

    reorg and cleanup

---
+++ pipeline/deploy_pipeline_infra.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+function usage() {
+cat <<EOM
+Usage: `basename $0` --token [value]
+Arguments:
+    --token          OAuthToken used to communicate with Github
+EOM
+
+exit
+}
+
+function arg_parse() {
+if [[ $# -lt 1 ]] ; then
+    usage
+fi
+
+# Parse Args
+while [[ $# -gt 1 ]]
+do
+    ARG="$1"
+
+    case $ARG in
+        --token)
+        TOKEN="$2"
+        shift
+        ;;
+        *)
+        usage
+        ;;
+    esac
+    shift
+done
+}
+
+function cfn_deploy() {
+STACK_NAME="thebestest-pipeline-infra"
+
+echo "## Validate"
+aws cloudformation validate-template --template-body file://pipeline_infra.yaml
+
+echo "## Package"
+aws cloudformation package  --template-file pipeline_infra.yaml \
+                            --s3-bucket thebestest-pipeline \
+                            --output-template-file pipeline_infra_deploy.yaml
+
+echo "## Deploy"
+aws cloudformation deploy   --template-file pipeline_infra_deploy.yaml \
+                            --stack-name ${STACK_NAME} \
+                            --capabilities CAPABILITY_IAM \
+                            --parameter-overrides "OAuthToken"=${TOKEN}
+}
+
+
+arg_parse $@
+cfn_deploy
