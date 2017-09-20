f2f456158d64c7713d72d1b21bc6229a302f9fef
NimbusScale <joe@nimbusscale.com>
2017-09-20 14:15:54 -0700

    apigw_stage_deployer custom cfn resource

--- pipeline/deploy_pipeline_infra.sh
+++ pipeline/deploy_pipeline_infra.sh
@@ -5,13 +5,17 @@ function usage() {
 cat <<EOM
 Usage: `basename $0` --token [value]
 Arguments:
-    --token          OAuthToken used to communicate with Github
+    --token         OAuthToken used to communicate with Github
+                    (Only needed for stack creation or to update the token)
+
 EOM
 
 exit
 }
 
 function arg_parse() {
+STACK_NAME="thebestest-pipeline-infra"
+
 while [[ $# -gt 1 ]]
 do
     ARG="$1"
@@ -40,8 +44,6 @@ pip install github3.py -t ./functions_deploy/pipeline_mgr/
 }
 
 function cfn_deploy() {
-STACK_NAME="thebestest-pipeline-infra"
-
 echo "## Validate"
 aws cloudformation validate-template --template-body file://pipeline_infra.yaml
 
