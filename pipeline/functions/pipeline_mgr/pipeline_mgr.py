85dd55e4e7a51f2e7874928094f9c906191cc358
NimbusScale <joe@nimbusscale.com>
2017-09-28 16:11:14 -0700

    start unit test on PR change

--- pipeline/functions/pipeline_mgr/pipeline_mgr.py
+++ pipeline/functions/pipeline_mgr/pipeline_mgr.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# webhook test 14
 import logging
 import os
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -10,37 +9,8 @@ import github3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+"""Helper Functions"""
 
-def webhook_handler(event, context):
-    """Lambda that evaluates the input provided by the API GW and determines
-    if it's a valid PR Open Webhook notice.
-    """
-    pipeline_info = {}
-    try:
-        pull_request = event['pull_request']
-        pr_info = {}
-        pr_info['owner'] = pull_request['head']['repo']['owner']['login']
-        pr_info['repo'] = pull_request['head']['repo']['name']
-        pr_info['url'] = pull_request['url']
-        pr_info['title'] = pull_request['title']
-        pr_info['branch'] = pull_request['head']['ref']
-        pr_info['sha'] = pull_request['head']['sha']
-    except:
-        logger.error(event)
-        raise Exception("Invalid Github PR Webhook")
-    pipeline_info['pr_info'] = pr_info
-
-    pipeline_action = "none"
-    if event['action'] in ['opened', 'synchronize']:
-        pipeline_action = "test"
-    elif event['action'] == 'closed' and event['pull_request']['merged']:
-        pipeline_action = "deploy"
-    pipeline_info['action'] = pipeline_action
-    logger.info("{} for PR {} ({}) of branch {}".format(pipeline_action,
-                                                         pr_info['title'],
-                                                         pr_info['url'],
-                                                         pr_info['branch']))
-    return pipeline_info
 
 def repackage_source(src_zip_path, dest_zip_path):
     """Repackages a zipball retrieved from Github to be as expected by
@@ -59,6 +29,43 @@ def repackage_source(src_zip_path, dest_zip_path):
                 src_path = os.path.join(root, file)
                 arc_path = src_path.replace(unzip_dir, '')
                 dest_zip.write(src_path, arcname=arc_path)
+
+
+"""Lambda's"""
+
+
+def webhook_handler(event, context):
+    """Lambda that evaluates the input provided by the API GW and determines
+    if it's a valid PR Open Webhook notice.
+    """
+    pipeline_info = {}
+    try:
+        pull_request = event['pull_request']
+        pr_info = {
+            'owner': pull_request['head']['repo']['owner']['login'],
+            'repo': pull_request['head']['repo']['name'],
+            'url': pull_request['url'],
+            'title': pull_request['title'],
+            'branch': pull_request['head']['ref'],
+            'sha': pull_request['head']['sha']
+        }
+    except:
+        logger.error(event)
+        raise Exception("Invalid Github PR Webhook")
+    pipeline_info['pr_info'] = pr_info
+
+    pipeline_action = "none"
+    if event['action'] in ['opened', 'synchronize']:
+        pipeline_action = "test"
+    elif event['action'] == 'closed' and event['pull_request']['merged']:
+        pipeline_action = "deploy"
+    pipeline_info['action'] = pipeline_action
+    logger.info("{} for PR {} ({}) of branch {}".format(pipeline_action,
+                                                         pr_info['title'],
+                                                         pr_info['url'],
+                                                         pr_info['branch']))
+    return pipeline_info
+
 
 def retrieve_source(event, context):
     """Lambda that retrieves a zipball of source based on a SHA and uploads
@@ -87,7 +94,7 @@ def retrieve_source(event, context):
         response = s3.put_object(Bucket=bucket,
                                  Key=zipball_name,
                                  Body=zipball)
-    etag = response['ETag']
+    event['version_id'] = response['VersionId']
     logger.info("Upload has ETag {}".format(etag))
     # Cleanup
     logger.info("Removing {}".format(download_path))
@@ -95,8 +102,35 @@ def retrieve_source(event, context):
     logger.info("Removing {}".format(zipball_path))
     os.remove(zipball_path)
     # Return
-    event['etag'] = etag
     return event
+
+
+def start_pipeline(event, context):
+    """Lambda that starts a codepipeline.
+
+    Pipeline name is provided by Env Var
+    """
+    owner = event['pr_info']['owner']
+    repo_name = event['pr_info']['repo']
+    sha = event['pr_info']['sha']
+    url = event['pr_info']['url']
+    pipeline_name = os.environ['PIPELINE_NAME']
+    token = os.environ['OAUTH_TOKEN']
+    codepipeline = boto3.client('codepipeline')
+    response = codepipeline.start_pipeline_execution(name=pipeline_name)
+    execution_id = response['pipelineExecutionId']
+    # Set status on the PR in Github
+    gh = github3.login(token=token)
+    repo = gh.repository(owner, repo_name)
+    repo.create_status(sha, 'pending',
+                       target_url=url,
+                       context='UnitTest',
+                       description= execution_id)
+    event['pipelineExecutionId'] = execution_id
+    return event
+
+
+
 
 
 
