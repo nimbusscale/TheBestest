e5bfb9b5c03658e94f7af03e1251d710651a2aad
NimbusScale <joe@nimbusscale.com>
2017-10-15 16:37:30 -0700

    use cfn waiter to wait for stack creation

--- pipeline/functions/pipeline/pipeline_mgr/stack.py
+++ pipeline/functions/pipeline/pipeline_mgr/stack.py
@@ -2,7 +2,7 @@ import logging
 import time
 
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, WaiterError
 
 logger = logging.getLogger()
 
@@ -39,32 +39,28 @@ class Stack:
             )
         stack_id = response['StackId']
         logger.info("StackId {}".format(stack_id))
-        wait = 0
-        max_wait = 180
-        sleep_seconds = 10
-        logger.info("Waiting up to {} for stack creation.".format(max_wait))
-        while wait <= max_wait:
-            time.sleep(sleep_seconds)
-            status = self.status
-            logger.info("stack status is {}".format(status))
-            if status == 'CREATE_COMPLETE':
-                break
-            elif status == 'CREATE_IN_PROGRESS':
-                wait = wait + sleep_seconds
-            elif (status == 'ROLLBACK_COMPLETE'
-                  or status == 'ROLLBACK_IN_PROGRESS'):
-                events_response = self.cfn.describe_stack_events(
-                        StackName=self.name)
-                for event in events_response['StackEvents']:
-                    if event['ResourceStatus'] == 'CREATE_FAILED':
-                        logger.error(event['ResourceStatusReason'])
-                raise RuntimeError(
-                    "Stack failed to create with status {}".format(
-                        self.status))
-            else:
-                raise RuntimeError(
-                    "Stack returned unhandled status of {}".format(
-                        self.status))
+        create_waiter = self.cfn.get_waiter('stack_create_complete')
+        waiter_delay = 10
+        waiter_max_attempts = 18
+        logger.info("Waiting up to {} seconds for stack creation.".format(
+            str(waiter_delay * waiter_max_attempts)
+        ))
+        try:
+            create_waiter.wait(StackName=stack_id,
+                        WaiterConfig={
+                            'Delay': waiter_delay,
+                            'MaxAttempts': waiter_max_attempts
+                            }
+                        )
+        except WaiterError:
+            events_response = self.cfn.describe_stack_events(
+                StackName=self.name)
+            for event in events_response['StackEvents']:
+                if event['ResourceStatus'] == 'CREATE_FAILED':
+                    logger.error(event['ResourceStatusReason'])
+            raise RuntimeError(
+                "Stack failed to create with status {}".format(
+                    self.status))
 
     def delete(self):
         self.cfn.delete_stack(StackName=self.arn)
